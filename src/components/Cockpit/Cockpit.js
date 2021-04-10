56817611b167dcde16116052bc68884f101b930e
Quan Tran <tran.q90@gmail.com>
2021-04-09 22:38:14 -0700

    useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css'
 
 const cockpit = (props) => {
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        //http request...
+    })
+    
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
