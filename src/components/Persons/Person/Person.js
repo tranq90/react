b773ad8247aaaf7c9d32d16c851a08d6c40f6236
Quan Tran <tran.q90@gmail.com>
2021-04-11 19:47:16 -0700

    setting state correctly

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary';
-import classes from './Person.css'
+import withClass from '../../../hoc/withClass';
+import classes from './Person.css';
 // const StyledDiv = styled.div`
 //     width: 60%;
 //     margin: 16px auto;
@@ -18,13 +19,13 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...]')
         return (
-            <React.Fragment>
+            <Aux>
                 <p key="1" onClick={this.props.click}>I'm {this.props.name} person and I am {this.props.age} years old!</p>
                 <p key="2">{this.props.children}</p>
                 <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
-            </React.Fragment >
+            </Aux>
         );
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -33,7 +33,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -83,7 +84,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter++
+      }
+    })
   }
 
   togglePersonsHandler = () => {
